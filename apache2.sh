673675b89fb62f99556f22e7717e9719058e19b8
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2023-05-21 13:10:27 -0300

    atualiza script

--- apache2.sh
+++ apache2.sh
@@ -1,23 +1,23 @@
 #! /bin/bash
 
 echo "Atualizando servidor..."
-apt-get update
-apt-get upgrade -y
+sudo apt-get update
+sudo apt-get upgrade -y
 echo "Atualizado!"
 
 echo "Instalando Apache2..."
-apt-get install apache2 -y
+sudo apt-get install apache2 -y
 echo "Instalado Apache2!"
 
 echo "Instalando Unzip..."
-apt-get install unzip -y
+sudo apt-get install unzip -y
 echo "Instalado Unzip!"
 
 echo "Baixando e copiando os arquivos da aplicação..."
 cd /tmp
-wget https://github.com/Luanftg/test-web-build/blob/main/build-web.zip
-unzip build-web.zip
-cd build-web
+sudo wget https://github.com/Luanftg/test-web-build/releases/download/artifact-flutter-web/web.zip
+unzip web.zip
+cd web
 cp -R * /var/www/html/
 echo "Arquivos copiados!"
 echo "Script Finalizado."
